c26a7de6fbf3f24ea3a4a9ed22a8da780ee572f1
kumaraguru M <kumaragurumurugan13@gmail.com>
2022-11-03 12:50:57 +0530

    version 0.1

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'guru', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'guru', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'guru', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'guru', 'Learn DevOps', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10005,'kumaraguru', 'Learn DevOps', CURRENT_DATE(), false);
